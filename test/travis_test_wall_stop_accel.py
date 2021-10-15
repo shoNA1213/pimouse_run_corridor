a9c692fe501c0b7d656dc2e16ffe7f2a41d43869
Tomoki Shoda <k8111exw@sit.ac.jp>
2021-10-15 05:27:40 +0000

    Add a wall_stop_accel

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -21,7 +21,7 @@ class WallStopAccelTest(unittest.TestCase):
         left, right = self.set_and_get(400,100,100,0) #total: 600
         self.assertTrue(left == right == 0,"can't stop")
 
-        left, right = self.set_and_get(40,0,0,9) #toal: 49
+        left, right = self.set_and_get(40,0,0,9) #total: 49
         self.assertTrue(0 < left == right < 1000, "can't move again")
 
         time.sleep(5.0)
@@ -29,7 +29,7 @@ class WallStopAccelTest(unittest.TestCase):
         self.assertTrue(2000 < left == right, "can't accerelate")
 
         left, right = self.set_and_get(15,0,20,15) #total: 50
-        self.assertTrue(left == right == 0,"can't stop again")
+        self.assertTrue(left == right == 0, "can't stop again")
 
 if __name__ == '__main__':
     time.sleep(3)
