e2ca310bd11ab0b5345aba9804eb0b1c113b6694
Tomoki Shoda <k8111exw@sit.ac.jp>
2021-10-15 05:18:12 +0000

    Add a wall_stop_accel

--- scripts/wall_stop_accel.py
+++ scripts/wall_stop_accel.py
@@ -23,15 +23,18 @@ class WallStop():
         while not rospy.is_shutdown():
             data.linear.x += accel
 
-            if self.sensor_values.sum_all >= 50: data.linear.x = 0.0
-            elif data.linear.x <= 0.2:           data.linear.x = 0.2
-            elif data.linear.x >= 0.8:           data.linear.x = 0.8
+            if self.sensor_values.sum_all > 50:
+                data.linear.x = 0.0
+            elif data.linear.x <= 0.2:
+                data.linear.x = 0.2
+            elif data.linear.x >= 0.8:          
+                data.linear.x = 0.8
 
             self.cmd_vel.publish(data)
             rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('wall_stop_accel')
+    rospy.init_node('wall_stop')
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
